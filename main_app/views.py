cc5a8d2b0fd633742e6b9e1454ac8fe64027eb95
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-08-25 21:46:42 -0600

    implement basic styles on home view

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>The Mighty Mighty Bosstones</h1>')
+    return render(request, 'home.html')
