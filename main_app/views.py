bb8d7d5ebc258e32f2ab7b7d074b826661a0f159
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-08-26 21:35:17 -0600

    add detail view for each record

--- main_app/views.py
+++ main_app/views.py
@@ -13,3 +13,7 @@ def about(request):
 def records_index(request):
     records = Record.objects.all()
     return render(request, 'records/index.html', {'records': records})
+
+def records_detail(request, record_id):
+    record = Record.objects.get(id=record_id)
+    return render(request, 'records/detail.html', {'record': record})
