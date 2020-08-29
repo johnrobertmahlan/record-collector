5f705d86fdfdaa45a49b23b557ec0480a0721306
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-08-29 15:35:56 -0600

    update to class-based views and implement add record functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('records/', views.records_index, name='index'),
-    path('records/<int:record_id>/', views.records_detail, name='detail')
+    # path('records/', views.records_index, name='index'),
+    path('records/', views.RecordList.as_view(), name='index'),
+    path('records/create', views.RecordCreate.as_view(), name='records_create'),
+    # path('records/<int:record_id>/', views.records_detail, name='detail'),
+    path('records/<int:pk>/', views.RecordDetail.as_view(), name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Record
 
 # Create your views here.
@@ -10,10 +13,20 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def records_index(request):
-    records = Record.objects.all()
-    return render(request, 'records/index.html', {'records': records})
+class RecordList(ListView):
+    model = Record
 
-def records_detail(request, record_id):
-    record = Record.objects.get(id=record_id)
-    return render(request, 'records/detail.html', {'record': record})
+# def records_index(request):
+#     records = Record.objects.all()
+#     return render(request, 'records/index.html', {'records': records})
+
+class RecordDetail(DetailView):
+    model = Record
+
+# def records_detail(request, record_id):
+#     record = Record.objects.get(id=record_id)
+#     return render(request, 'records/detail.html', {'record': record})
+
+class RecordCreate(CreateView):
+    model = Record
+    fields = '__all__'
